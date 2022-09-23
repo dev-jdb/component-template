4b43ed997361831638a16df51a9b53b33cbf3b20
John Blackwell <jblackwell072588@gmail.com>
2022-09-23 11:53:14 -0700

    fix: Add an example Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+
+import { StyledButton } from "./styles";
+import { ButtonProps } from "./types";
+
+export const Button: FC<ButtonProps> = ({
+  size,
+  primary,
+  disabled,
+  text,
+  onClick,
+  ...props
+}) => {
+  return (
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      primary={primary}
+      disabled={disabled}
+      size={size}
+      {...props}
+    >
+      {text}
+    </StyledButton>
+  );
+};
+
+export default Button;
