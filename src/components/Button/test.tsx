e31d45897e598297f6ad7d5eefa6fef23171b9af
John Blackwell <jblackwell072588@gmail.com>
2023-04-21 15:57:02 -0700

    fix: Get tests working

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -5,10 +5,8 @@ import { render, screen } from '@testing-library/react';
 import Button from './';
 
 describe('Running Test for Button', () => {
-	test('Check Button Disabled', () => {
-		render(<Button text='Button' disabled />);
-		expect(
-			screen.getByRole('button', { name: 'Button' })
-		).toBeDisabled();
-	});
+  test('Check Button Disabled', () => {
+    render(<Button text='Button' disabled />);
+    expect(screen.getByRole('button', { name: 'Button' })).toBeDisabled();
+  });
 });
